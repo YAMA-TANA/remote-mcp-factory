2e23c0a0448c47cedbe63e83032af840cd9fcbb3
YAMA-TANA <renwushaojieshijieshi@gmail.com>
2026-09-16 09:21:35 +0900

    feat: add localized mock routes

---
+++ web/app/[locale]/mock/page.tsx
@@ -0,0 +1,9 @@
+import MockPage from '../../mock/page';
+import { localeMetadata, parseLocaleParam } from '../../seo';
+
+export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
+  return localeMetadata(parseLocaleParam(locale), 'mock');
+}
+
+export default MockPage;
