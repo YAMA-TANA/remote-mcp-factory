1d0b5a4497f38f720f47b1b5bb1292e0adb1fb20
YAMA-TANA <renwushaojieshijieshi@gmail.com>
2026-09-17 19:03:07 +0900

    Expose exports in License and Flags managers

--- web/app/service-resource-detail.tsx
+++ web/app/service-resource-detail.tsx
@@ -7,6 +7,7 @@ import FormsManagementDetail from './service-forms-management';
 import JsonManagementDetail from './service-json-management';
 import LicenseManagementDetail from './service-license-management';
 import LegacyServiceResourceDetail from './service-resource-detail-legacy';
+import ServiceResourceExport from './service-resource-export';
 
 type Props = {
   service: GenericServiceSlug;
@@ -17,12 +18,19 @@ type Props = {
   copyValue: (value: string) => Promise<void>;
 };
 
+function LicenseWorkspace(props: Props) {
+  return <div className="advancedManagementStack"><LicenseManagementDetail {...props} /><ServiceResourceExport service="license" resource={props.resource} api={props.api} /></div>;
+}
+function FlagsWorkspace(props: Props) {
+  return <div className="advancedManagementStack"><FlagsManagementDetail {...props} /><ServiceResourceExport service="flags" resource={props.resource} api={props.api} /></div>;
+}
+
 /** Route specialized management without changing other service workflows. */
 export default function ServiceResourceDetail(props: Props) {
   if (props.service === 'files') return <FilesManagementDetail {...props} />;
   if (props.service === 'json') return <JsonManagementDetail {...props} />;
   if (props.service === 'forms') return <FormsManagementDetail {...props} />;
-  if (props.service === 'license') return <LicenseManagementDetail {...props} />;
-  if (props.service === 'flags') return <FlagsManagementDetail {...props} />;
+  if (props.service === 'license') return <LicenseWorkspace {...props} />;
+  if (props.service === 'flags') return <FlagsWorkspace {...props} />;
   return <LegacyServiceResourceDetail {...props} />;
 }
